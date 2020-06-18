ef44aa9afa10a0362be423eeca24e0ea9ac22d99
Jan Wojcicki <janwojcicki45@gmail.com>
2020-06-18 05:56:06 +0200

    variables

--- src/com/lox/Interpreter.java
+++ src/com/lox/Interpreter.java
@@ -3,6 +3,7 @@ package com.lox;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+	private Environment environment = new Environment();
 
 	@Override
 	public Void visitExpressionStmt(Stmt.Expression stmt) {
@@ -14,6 +15,23 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	public Void visitPrintStmt(Stmt.Print stmt) {
 		Object value = evaluate(stmt.expression);
 		System.out.println(stringify(value));
+		return null;
+	}
+
+	@Override
+	public Void visitVarStmt(Stmt.Var stmt) {
+		Object value = null;
+		if (stmt.initializer != null) {
+			value = evaluate(stmt.initializer);
+		}
+
+		environment.define(stmt.name.lexeme, value);
+		return null;
+	}
+
+	@Override
+	public Void visitBlockStmt(Stmt.Block stmt) {
+		executeBlock(stmt.statements, new Environment(environment));
 		return null;
 	}
 
@@ -89,6 +107,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	}
 
 	@Override
+	public Object visitVariableExpr(Expr.Variable expr) {
+		return environment.get(expr.name);
+	}
+
+	@Override
+	public Object visitAssignExpr(Expr.Assign expr) {
+		Object value = evaluate(expr.value);
+
+		environment.assign(expr.name, value);
+		return value;
+	}
+
+	@Override
 	public Object visitCommaExpr(Expr.Comma expr) {
 		evaluate(expr.left);
 		return evaluate(expr.right);
@@ -106,6 +137,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	private void execute(Stmt stmt) {
 		stmt.accept(this);
+	}
+
+	void executeBlock(List<Stmt> statements, Environment environment) {
+		Environment previous = this.environment;
+		try {
+			this.environment = environment;
+
+			for (Stmt statement : statements) {
+				execute(statement);
+			}
+		} finally {
+			this.environment = previous;
+		}
 	}
 
 	private void checkNumberOperand(Token operator, Object operand) {
